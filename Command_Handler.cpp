93a0a562f10c580e34f451ba709c1525235bec55
Comyar Dehlavi <comyar2007@gmail,com>
2021-05-24 23:40:13 -0500

    Added different waves and wave properties

--- Command_Handler.cpp
+++ Command_Handler.cpp
@@ -2,12 +2,45 @@
 #include "Config_Display.h"
 #include <iostream>
 #include <string>
+#include <windows.h>
+
+std::string removeWord(std::string str, std::string word){
+	if (str.find(word) != std::string::npos)
+	{
+		size_t p = -1;
+		std::string tempWord = word + " ";
+
+		while ((p = str.find(word)) != std::string::npos)
+			str.replace(p, tempWord.length(), "");
+
+		tempWord = " " + word;
+
+		while ((p = str.find(word)) != std::string::npos)
+			str.replace(p, tempWord.length(), "");
+	}
+
+	return str;
+}
+
+std::string removeSpaces(std::string str){
+	int count = 0;
+
+	for (int i = 0; str[i]; i++)
+		if (str[i] != ' ')
+			str[count++] = str[i];
+
+	str[count] = '\0';
+	return str;
+}
 
 std::string Command_Handler::start_session() {
 	while (true) {
 		std::string in = "";
 		std::getline(std::cin, in);
 		
+		if (in == "clear")
+			system("CLS");
+
 		if (in == "-on") {
 			connection_status = true;
 		}
@@ -16,16 +49,51 @@ std::string Command_Handler::start_session() {
 			connection_status = false;
 		}
 
+		if (in.find("-amplitude") != std::string::npos) {
+			try {
+				removeSpaces(in);
+				AMPLITUDE = std::stoi(removeWord(in, "-amplitude"));
+				std::cout << "Set amplitude to " << removeWord(in, "-amplitude") << std::endl;
+				status_changed = true;
+			}
+			catch (int e) {
+				std::cout << "Exception Nr." << e << " Has occured. It could be linked to incorrect syntax. Try rewritting the command?\n";
+			}
+		}
+		else if (in.find("-frequency") != std::string::npos) {
+			try {
+				removeSpaces(in);
+				increment = (float)std::stoi(removeWord(in, "-frequency")) / SAMPLE_RATE;
+				std::cout << "Set frequency to " << removeWord(in, "-frequency") << std::endl;
+				status_changed = true;
+			}
+			catch (int e) {
+				std::cout << "Exception Nr." << e << " Has occured. It could be linked to incorrect syntax. Try rewritting the command?\n";
+			}
+		}
+		else if (in.find("-wave") != std::string::npos) {
+			try {
+				removeSpaces(in);
+				wave = std::stoi(removeWord(in, "-wave"));
+				std::cout << "Set wave to " << removeWord(in, "-wave") << std::endl;
+				status_changed = true;
+			}
+			catch (int e) {
+				std::cout << "Exception Nr." << e << " Has occured. It could be linked to incorrect syntax. Try rewritting the command?\n";
+			}
+		}
 
-		if (status_changed == true) {
+		if (status_changed == true && connection_status == true) {
 		    End_Wave();
 			Generate_Wave();
-			status_changed == false;
+
+			currently_connected = true;
+			status_changed = false;
 		}
 
 		else if (connection_status == true && currently_connected == false) {
 			Generate_Wave();
-			std::cout << "Sine wave generated from selected settings!\n";
+			std::cout << "Wave generated from selected settings!\n";
 			currently_connected = true;
 		}
 		else if (connection_status == false && currently_connected == true) {
--- Wave_Generator.cpp
+++ Wave_Generator.cpp
@@ -2,36 +2,49 @@
 #include <cmath>
 #include <iostream>
 
+float mod(float a, float b) {
+	return a - (long long)(a / b) * b;
+}
+
+float pingpong(float x, float period) {
+	auto a = mod(x, period);
+	return (a < period / 2 ? a : period - a);
+}
+
 void Wave_Generator::Generate_Wave() {
 	const unsigned SAMPLES = 44100;
 
 	sf::Int16 raw[SAMPLES];
 	
-	if (wave > 0) {
+	if (wave > 3) {
 		wave = 0;
 	}
+	//Sine
 	if (wave == 0) {
-		for (unsigned i = 0; i < SAMPLES; i++) {
+		for (unsigned i = 0; i < SAMPLES ; i++) {
 			raw[i] = AMPLITUDE * sin(x * TWO_PI);
 			x += increment;
 		}
 	}
+	//Square
 	else if (wave == 1) {
 		for (unsigned i = 0; i < SAMPLES; i++) {
-			raw[i] = AMPLITUDE * sin(x * TWO_PI);
-			x += increment;
+			raw[i] = AMPLITUDE * (sin(x * TWO_PI) < 0 ? -1 : 1);
+			x += 5 * increment;
 		}
 	}
+	//Noise
 	else  if (wave == 2) {
 		for (unsigned i = 0; i < SAMPLES; i++) {
-			raw[i] = AMPLITUDE * sin(x * TWO_PI);
-			x += increment;
+			raw[i] = 100000000 * sin(x * TWO_PI);
+			x += 100000000 * increment;
 		}
-	}
+	} 
+	//Triangle
 	else if (wave == 3) {
-		for (unsigned i = 0; i < SAMPLES; i++) {
-			raw[i] = AMPLITUDE * sin(x * TWO_PI);
-			x += increment;
+		for (unsigned i = 0; i < SAMPLES ; i++) {
+			raw[i] = AMPLITUDE * (2 / 3.14159 * pingpong(x, TWO_PI) - 1);
+			x += 40 * increment;
 		}
 	}
 
